69f6779b40a18a1a1be7e6a1ab4a2e39545613ad
Pedro Martins <pedro.martins62@yahoo.com>
2023-06-18 17:06:38 -0300

    Cypress Basico V2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,7 +1,7 @@
 // CAC-TAT.spec.js created with Cypress
 //
 // Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
+// If you're unfamiliar with how Cypress works
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,7 +20,6 @@
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
 //
-//
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
